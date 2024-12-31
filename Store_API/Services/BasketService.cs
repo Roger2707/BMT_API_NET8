e424090a162b7accf62d14ad5d040bb777f13cfb
Roger2707 <rogerhuynh2707@gmail.com>
2024-12-31 16:25:21 +0700

    Update Sql to File (Upsert Basket)

--- Store_API/Services/BasketService.cs
+++ Store_API/Services/BasketService.cs
@@ -77,64 +77,8 @@ namespace Store_API.Services
         #region Actions
         public async Task HandleBasketMode(int userId, int productId, bool mode)
         {
-            string query = @"
-                    DECLARE @BasketId INT
-                    DECLARE @BasketItemId INT
-                    DECLARE @OldQuantity INT
-
-                    BEGIN TRY
-	                    BEGIN TRANSACTION
-		                    IF(NOT EXISTS(SELECT Id FROM Baskets WHERE UserId = @UserId)) 
-			                    BEGIN
-				                    INSERT INTO Baskets (UserId) VALUES (@UserId)
-
-				                    INSERT INTO BasketItems (BasketId, ProductId, Quantity, Status) 
-                                    VALUES (SCOPE_IDENTITY(), @ProductId, 1, 0)
-			                    END
-		                    ELSE
-			                    BEGIN
-				                    IF(@Mode = 1)
-					                    BEGIN
-						                    SELECT @BasketId = Id FROM Baskets WHERE UserId = @UserId
-						                    IF(EXISTS(SELECT Id FROM BasketItems WHERE ProductId = @ProductId AND BasketId = @BasketId))
-							                    BEGIN
-								                    SELECT @BasketItemId = Id, @OldQuantity = Quantity 
-                                                    FROM BasketItems 
-                                                    WHERE ProductId = @ProductId AND BasketId = @BasketId
-
-								                    UPDATE BasketItems SET Quantity = @OldQuantity + 1 WHERE Id = @BasketItemId
-							                    END
-						                    ELSE
-							                    BEGIN
-								                    INSERT INTO BasketItems (BasketId, ProductId, Quantity, Status) VALUES (@BasketItemId, @ProductId, 1, 0)
-							                    END
-					                    END
-				                    ELSE
-					                    BEGIN
-						                    SELECT @BasketId = Id FROM Baskets WHERE UserId = @UserId
-						                    SELECT @BasketItemId = Id, @OldQuantity = Quantity FROM BasketItems WHERE ProductId = @ProductId AND BasketId = @BasketId
-
-						                    IF(@OldQuantity > 1)
-							                    BEGIN
-								                    UPDATE BasketItems SET Quantity = @OldQuantity - 1 WHERE Id = @BasketItemId
-							                    END
-						                    ELSE
-							                    BEGIN
-								                    DELETE BasketItems WHERE Id = @BasketItemId
-							                    END
-					                    END
-			                    END
-	                    COMMIT;
-                    END TRY
-
-                    BEGIN CATCH
-	                    PRINT 'An error occurred. Rolling back the transaction';
-	                    IF @@TRANCOUNT > 0
-                            ROLLBACK;
-                    END CATCH
-
-";
-
+            string sqlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sql", "upsertbasket.sql");
+            string query = System.IO.File.ReadAllText(sqlFilePath);
             try
             {
                 var p = new { UserId = userId, ProductId = productId, Mode = mode };
