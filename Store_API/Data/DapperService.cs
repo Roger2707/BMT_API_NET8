5f9b0e157844adbf1d135b149468b76da2e1acc7
Roger <rogerhuynh2707@gmail.com>
2025-04-18 11:41:28 +0700

    Update Connection Dapper

--- Store_API/Data/DapperService.cs
+++ Store_API/Data/DapperService.cs
@@ -1,6 +1,5 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
-using static Org.BouncyCastle.Math.EC.ECCurve;
 
 namespace Store_API.Data
 {
@@ -46,14 +45,14 @@ namespace Store_API.Data
 
         public async Task<List<TResult>> QueryAsync<TResult>(string query, object p = null)
         {
-            await EnsureConnectionAsync();
-            return (await _connection.QueryAsync<TResult>(query, p)).ToList();
+            var connection = await EnsureConnectionAsync();
+            return (await connection.QueryAsync<TResult>(query, p)).ToList();
         }    
 
         public async Task<TResult> QueryFirstOrDefaultAsync<TResult>(string query, object p = null)
         {
-            await EnsureConnectionAsync();
-            return await _connection.QueryFirstOrDefaultAsync<TResult>(query, p);
+            var connection = await EnsureConnectionAsync();
+            return await connection.QueryFirstOrDefaultAsync<TResult>(query, p);
         }
 
         public async Task<int> Execute(string query, object p = null) => await _connection.ExecuteAsync(query, p, _transaction);
